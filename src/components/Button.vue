f264db674c3252ba25e96d47659d577dd049d743
emanuelecardone <e_m.cardone@yahoo.it>
2022-01-17 11:20:24 +0100

    Fatti alcuni stili di base

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <a href="#" class="btn page_btn"></a>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String
+    }    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/mixins.scss';
+
+    .page_btn{
+        @include page_btn;
+    }
+</style>
