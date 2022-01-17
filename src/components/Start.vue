22224372c59492ee5a90c6c53cc2b945826b6ef9
emanuelecardone <e_m.cardone@yahoo.it>
2022-01-17 16:51:07 +0100

    Finita sezione Start

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,11 +1,31 @@
 <template>
     <section class="start">
         <Banner :type="'start'" :content="bannerData" />
-        <div class="infos_wrapper d-flex justify-content-between align-items-center mt_60">
-            <div v-for="info,index in infos" :key="index" class="single_info_wrapper p-4" :class="{'mx_5_percent': index === 1}">
+        <div class="infos_wrapper d-flex justify-content-between align-items-center mt_60 mb_120">
+            <div v-for="info,index in infos" :key="index" class="single_info_wrapper p-4">
                 <i :class="'info_icon fs_50 fas fa-' + info.icon"></i>
                 <h6 class="info_title my-4">{{info.title}}</h6>
                 <p class="info_text">{{info.text}}</p>
+            </div>
+        </div>
+        <div class="images_wrapper text-capitalize">
+            <div class="images_wrapper_left d-flex flex-column flex-wrap align-items-start position-relative mb_30">
+                <div v-for="image,index in images.left" :key="index" :class="{'align-self-end': index > 0, 'position-absolute right-0 bottom-0': index === 2}">
+                    <img :src="require('../assets/img/' + image.src + '.jpg')" :alt="'Immagine di ' + image.title">
+                    <div class="titles_img d-flex justify-content-between align-items-center my-1">
+                        <span class="image_title fs-5">{{image.title}}</span>
+                        <span class="image_subtitle fs-6">{{image.subtitle}}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="images_wrapper_right d-flex justify-content-between">
+                <div v-for="image,index in images.right" :key="index">
+                    <img :src="require('../assets/img/' + image.src + '.jpg')" :alt="'Immagine di ' + image.title">
+                    <div class="titles_img d-flex justify-content-between align-items-center my-1">
+                        <span class="image_title fs-5">{{image.title}}</span>
+                        <span class="image_subtitle fs-6">{{image.subtitle}}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -41,7 +61,44 @@ export default {
                     title: 'Graduate in as little as 1 Year',
                     text: 'Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy'
                 }
-            ]
+            ],
+            // Oggetto con src e test delle immagini
+            images: {
+                left: [
+                    {
+                        src: 'cat_1-770x375',
+                        title: 'software development',
+                        subtitle: '5 courses'
+                    },
+                    {
+                        src: 'cat_5-370x155',
+                        title: 'music',
+                        subtitle: '5 courses'
+                    },
+                    {
+                        src: 'cat_4-370x155',
+                        title: 'exercise',
+                        subtitle: '7 courses'
+                    }
+                ],
+                right: [
+                    {
+                        src: 'cat_2-370x155',
+                        title: 'art',
+                        subtitle: '8 courses'
+                    },
+                    {
+                        src: 'cat_3-370x155',
+                        title: 'material design',
+                        subtitle: '5 courses'
+                    },
+                    {
+                        src: 'cat_6-370x155',
+                        title: 'photography',
+                        subtitle: '4 courses'
+                    }
+                ]
+            }
         };
     }    
 }
@@ -51,11 +108,11 @@ export default {
 @import '../style/variables.scss';
 
     .start{
-        padding: 0 350px;
+        padding: 0 375px;
     }
     .single_info_wrapper{
         background-color: #f2f6fb;
-        width: 30%;
+        width: 370px;
         height: 250px;
         display: flex;
         flex-direction: column;
@@ -68,6 +125,19 @@ export default {
         }
         .info_text{
             color: $tertiary_color;
+        }
+    }
+    .image_title{
+        color: $primary_color;
+    }
+    .image_subtitle{
+        color: $tertiary_color;
+    }
+    .images_wrapper_left{
+        max-height: 415px;
+
+        .last_img{
+            justify-self: flex-end;
         }
     }
 </style>
