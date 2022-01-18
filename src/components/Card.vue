cacad03ac10ad003fe7ea32f2b56317ee2cf9665
emanuelecardone <e_m.cardone@yahoo.it>
2022-01-18 14:11:03 +0100

    Finite e stampate le cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,14 @@
 <template>
     <!-- La card è divisa in 2 div, uno con l'immagine ed uno coi testi, i 2 div hanno stessa altezza e per questo mi occorre dare un'altezza fissa alla card -->
     <div class="courses_card">
-        <div class="card_img">
+        <div class="card_img position-relative">
             <img :src="require('../assets/img/' + infos.image)" :alt="'Immagine corso di ' + infos.category" class="w-100 h-100">
+            <div v-if="infos.attribute !== 'normal'" class="type_mark position-absolute text-uppercase" :class="infos.attribute">
+                {{infos.attribute}}
+            </div>
+            <div v-if="infos.featured" class="featured_mark position-absolute text-uppercase">
+                featured
+            </div>
         </div>
         <div class="card_infos d-flex flex-column justify-content-between" :class="{'featured': infos.featured}">
             <div class="card_infos_up">
@@ -56,12 +62,49 @@ export default {
             width: 100%;
             height: 50%;
 
+            &.card_img{
+                overflow: hidden;
+
+                img{
+                    z-index: 1000;
+                }
+                .type_mark{
+                    top: .5rem;
+                    right: .5rem;
+                    padding: .2rem .4rem;
+                    border-radius: 5px;
+                    font-size: 12px;
+                    color: white;
+                    z-index: 2000;
+
+                    &.new{
+                        background-color: #4bd7a8;
+                    }
+                    &.hot{
+                        background-color: #fd0303;
+                    }
+                    &.special{
+                        background-color: #f09d28;
+                    }
+                }
+                .featured_mark{
+                    color: white;
+                    background-color: #f09d28;
+                    padding: .3rem 5rem;
+                    transform: rotate(315deg);
+                    font-size: 10px;
+                    top: 1.2rem;
+                    left: -4.5rem;
+                    z-index: 2000;
+                }
+            }
+
             &.card_infos{
                 border: 1px solid $tertiary_color;
                 padding: .5rem;
 
                 &.featured{
-                    border: 1px solid #fecc4f;
+                    border: 1px solid #f09d28;
                     border-top: none;
                 }
                 span{
